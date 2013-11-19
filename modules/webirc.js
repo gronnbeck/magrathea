03cce34009c1ff276bb219018613dbc95ffc467c
Ken Grønnbeck <ken.gronnbeck@bekk.no>
2013-11-19 12:10:06 +0100

    Rewrting how config is loaded

--- modules/webirc.js
+++ modules/webirc.js
@@ -1,10 +1,19 @@
-var cli = require('./client');
+var cli = require('./client')
+, _ = require('underscore');
 
-exports.start = function() {
+var default_config = {};
+
+exports.start = function(_config) {
+
+	start(_.defaults(config, default_config));
+
+};
+
+var start = function(config) {
 
 	var ws = require('ws')
-		, WebSocketServer = ws.Server
-		, wss = new WebSocketServer({port: 1881});
+	, WebSocketServer = ws.Server
+	, wss = new WebSocketServer({port: 1881});
 
 
 	wss.on('connection', function(ws) {
@@ -13,9 +22,9 @@ exports.start = function() {
 		ws.send('Connection received. Looking for your IRC connections');
 
 		cli.proxyWebClient( 'test', '1337-hash', ws );
-	    ws.on('message', function(message) {
-	        console.log('received: %s', message);
-	    });
-	    
+		ws.on('message', function(message) {
+			console.log('received: %s', message);
+		});
+
 	});
-}
+};
