7ff024fce083e29f169629eee77ed06d6338e679
Ken Grønnbeck <ken.gronnbeck@bekk.no>
2013-12-13 22:04:33 +0100

    [test] What happens if a ws disconnects?

--- tests/unit/client-reconnects.tests.spec.js
+++ tests/unit/client-reconnects.tests.spec.js
@@ -1,41 +1,50 @@
 var modulePath = '../..'
 	, config = require(modulePath + '/config')
-	, client = require(modulePath + '/client')
+	, client = require(modulePath + '/modules/client')
 	, _ = require('underscore');
 
 describe('client', function() {
 
-	var cli, ws;
+	var cli, ws, ws_send, ws_listen, _callback;
 	beforeEach(function() {
-		cli = client.init({
-			send: function(message) {
-				
-			},
-			connect: function() {
-
-			},
-			listen: function(callback) {
-
-			},
-			__id: 'for-testing'
-		});	
-
+		
 		ws = {
 			send: function(message) {
-
+				ws_send(message);
 			},
 			listen: function(message) {
-
+				ws_listen(message);
 			}
 		}
+
+		var proxy = function(connConf) {
+			return {
+				send: function(message) {
+					ws.listen(message);				
+				},
+				connect: function() {
+					return this;
+				},
+				listen: function(callback) {
+					_callback = callback;
+				},
+				__id: 'for-testing'
+			};
+		}
+		cli = client.init(proxy);	
+
+		
 	});
 
 	it('should not fail when proxy disconnects', function() {
-
+		ws_listen = function(callback) {
+			console.log(callback);
+		};
+		ws_send = function(message) {
+			throw Error("not opened");
+		};
+		cli.proxyWebClient('test', '1337-hash', ws);
+		_callback("heyhey");
 	});
-
-	it('should not send message to a disconnected proxy', function() {
-
-	})
 
 });
