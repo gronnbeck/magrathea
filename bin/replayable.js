8db23b9304fbcc7bce2968ab76e279a8ca215ff6
Ken Grønnbeck <kengroenn@gmail.com>
2014-03-03 19:55:04 +0100

    (chore) jsHint corrcet spaces and style

--- bin/replayable.js
+++ bin/replayable.js
@@ -1,43 +1,43 @@
 var WebSocket = require('ws');
 
 var config = {
-	server: {
-		port: 8081
-	},
-	proxy: {
-		url: 'ws://localhost',
-		port: 8080
-	}
+  server: {
+    port: 8081
+  },
+  proxy: {
+    url: 'ws://localhost',
+    port: 8080
+  }
 };
 
 console.log('[SERVER] Starting Replayable websocket server');
 var server = new WebSocket.Server({ port: config.server.port });
 
-server.on('connection', function (client) {
-	console.log('[SERVER] Connection received');
+server.on('connection', function(client) {
+  console.log('[SERVER] Connection received');
 
-	console.log('[PROXY] Setting up proxy for client');
-	var proxy = new WebSocket(config.proxy.url + ':' + config.proxy.port);
+  console.log('[PROXY] Setting up proxy for client');
+  var proxy = new WebSocket(config.proxy.url + ':' + config.proxy.port);
 
-	proxy.on('open', function() {
-		console.log('[PROXY] Proxy discovered');
-	});
+  proxy.on('open', function() {
+    console.log('[PROXY] Proxy discovered');
+  });
 
-	proxy.on('message', function(data) {
-		client.send(data);
-	});
+  proxy.on('message', function(data) {
+    client.send(data);
+  });
 
-	proxy.on('close', function() {
-		console.log('Proxy closed. Handle reconnection.');
-	});
+  proxy.on('close', function() {
+    console.log('Proxy closed. Handle reconnection.');
+  });
 
-	client.on('message', function(data) {
-		proxy.send(data);
-	});
+  client.on('message', function(data) {
+    proxy.send(data);
+  });
 
-	client.on('close', function() {
-		proxy.close();
-	});
+  client.on('close', function() {
+    proxy.close();
+  });
 
 });
 
