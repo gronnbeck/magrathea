c4a2f5f7d7673e5284787f045b5cf5719831e632
Ken Grønnbeck <kengroenn@gmail.com>
2014-03-08 15:05:10 +0100

    (refactor) Moved proxy commands into Commands-module

---
+++ modules/commands.js
@@ -0,0 +1,43 @@
+exports.init = function(ws, connections) {
+	return {
+		connect: function(message) {
+			var config = message.connection
+			, connection = connections.create(config);
+			this.establish(connection);
+		},
+		reconnect: function(message) {
+			var key = message.key;
+			if (connections.has(key)) {
+				var connection = connections.get(key);
+				this.establish(connection);
+			} 
+			else {
+				ws.send(JSON.stringify({ 
+					success: false, 
+					type: 'disconnected', 
+					msg: 'The connection you are referencing does not exists'
+				}));
+			}
+		},
+		establish: function(connection) {
+			var client = connection.client;
+			ws.send( JSON.stringify({ type: 'connected', key: connection.key }) );
+
+			client.on('msg', function(msg) {
+				ws.send(JSON.stringify(msg));
+			});
+
+			ws.on('message', function (msg) {
+				client.emit('send', JSON.parse(msg));
+			});
+
+			ws.on('close', function() {
+				client.removeAllListeners();
+			});
+		},
+		disconnect: function(message) {
+			console.log('Disconenct not implemented. Exiting');
+			process.kill();
+		}
+	}	
+};
--- modules/proxy.js
+++ modules/proxy.js
@@ -1,6 +1,7 @@
 var _ = require('underscore')
 , webSocket = require('ws')
-, container = require('./container');
+, container = require('./container')
+, Commands = require('./commands');
 
 var defaults = {
 	port: 8080
@@ -13,54 +14,14 @@ exports.start = function(configure) {
 
 	console.log('Starting proxy');
 	wss.on('connection', function (ws) {
-		var commands = {
-			connect: function(message) {
-				var config = message.connection
-				, connection = connections.create(config);
-				this.establish(connection);
-			},
-			reconnect: function(message) {
-				var key = message.key;
-				if (connections.has(key)) {
-					var connection = connections.get(key);
-					this.establish(connection);
-				} 
-				else {
-					ws.send(JSON.stringify({ 
-						success: false, 
-						type: 'disconnected', 
-						msg: 'The connection you are referencing does not exists'
-					}));
-				}
-			},
-			establish: function(connection) {
-				var client = connection.client;
-				ws.send( JSON.stringify({ type: 'connected', key: connection.key }) );
-
-				client.on('msg', function(msg) {
-					ws.send(JSON.stringify(msg));
-				});
-
-				ws.on('message', function (msg) {
-					client.emit('send', JSON.parse(msg));
-				});
-
-				ws.on('close', function() {
-					client.removeAllListeners();
-				});
-			},
-			disconnect: function(message) {
-				console.log('Disconenct not implemented. Exiting');
-				process.kill();
-			}
-		};
+		var cmds = Commands.init(ws, connections);
 
 		console.log("Connection received from " + ws);
 		ws.on('message', function (msg) {
 			var message = JSON.parse(msg);
 
-			if (_.has(commands, message.type)) 
-				commands[message.type](message);
+			if (_.has(cmds, message.type)) 
+				cmds[message.type](message);
 			else {
 				ws.send(JSON.stringify({
 					success: false,
