c45e67d8b2e908ad49e03dcc875f6fa98f3ccb41
Ken Grønnbeck <kengroenn@gmail.com>
2014-03-12 19:32:57 +0100

    (refactor) Got lost while refactoring. Needs clean-up

--- bin/nano.js
+++ bin/nano.js
@@ -2,29 +2,35 @@
 
 var Save = require('../modules/save')
 , save = Save.init()
+, Q = require('q')
+, dbName = 'freenode_pokemen'
 
+var doIt = function() {
+  save.getdb(dbName)
+  .then(function success(db) {
+     return db
+   })
+   .then(function success(db) {
+     return db.channel('freenode', 'pokemen')
+   })
+   .then(function success(channel) {
+     channel.latest()
+     return channel.insert({ts: new Date(), from: 'me', payload: 'paaayload'})
+   })
+   .then(function success(inserted) {
+     console.log('im done: ' + JSON.stringify(inserted))
+   })
+   .catch(function(error) {
+     console.log('what?: ' + error)
+   })
 
-save.getdb('channel')
-.then(function success(db) {
-   return db
- }, function error(error) {
-   console.log('Error: ' + error)
- })
- .then(function success(db) {
-   return db.channel('freeonode', 'pokemen')
- }, function error(error) {
-   console.log('Error: ' + error)
- })
- .then(function success(channel) {
-   return channel.insert({ts: new Date(), from: 'me', payload: 'paaayload'})
- }, function error(error) {
-   console.log(error)
- })
- .then(function success(inserted) {
-   console.log('im done: ' + JSON.stringify(inserted))
- }, function (error) {
-   console.log(error)
- })
- .catch(function(error) {
-   console.log('what?: ' + error)
- })
+}
+
+save.createdb(dbName)
+.then(function() {
+  console.log('Did not exist. Creating db')
+  doIt()
+}, function() {
+  console.log('Finding already existing db')
+  doIt()
+})
--- modules/save.js
+++ modules/save.js
@@ -3,13 +3,50 @@ var Nano = require('nano')
 , _ = require('underscore')
 , uuid = require('node-uuid')
 
-exports.init = function(conf) {
-  var defaults = {
-    url: 'http://127.0.0.1:5984/'
+var saveChannel = function(network, chan, message) {
+  return {
+      network: network,
+      channel: chan,
+      from: message.from,
+      ts: message.ts,
+      payload: message.payload
   }
+}
+
+var channelModel = function(db, nano) {
+  return function(network, chan) {
+    var channel = nano.db.use(db)
+
+    function insert(message, deferred) {
+      channel.insert(saveChannel(network, chan, message)
+      , uuid.v4()
+      , function(err, body) {
+          if (err) deferred.reject(new Error(err))
+          else deferred.resolve(body)
+      })
+    }
+
+    return {
+      insert: function(message) {
+        var deferred = Q.defer()
+        insert(message, deferred)
+        return deferred.promise
+      },
+      latest: function() {
+        var deferred = Q.defer()
+        channel.list({descending: true}, function(err, body) {
+          console.log(err, body)
+        })
+        return deferred.promise
+      }
+    }
+  }
+}
+
+exports.init = function(conf) {
+  var defaults = { url: 'http://127.0.0.1:5984/' }
   , config = _.defaults(conf || {}, defaults)
   , nano = Nano(config)
-
 
   return {
     createdb: function(name) {
@@ -26,34 +63,9 @@ exports.init = function(conf) {
     getdb: function(name) {
       var deferred = Q.defer()
       nano.db.get(name, function(error, body) {
-        var channel = nano.db.use(name)
-
         if (error) deferred.reject(new Error(error))
         else deferred.resolve({
-
-          channel: function(network, chan) {
-
-            return {
-              insert: function(message) {
-                var deferred = Q.defer()
-
-                channel.insert({
-                    network: network,
-                    channel: chan,
-                    from: message.from,
-                    ts: message.ts,
-                    payload: message.payload
-                }
-                , uuid.v4()
-                , function(err, body) {
-                    if (err) deferred.reject(new Error(err))
-                    else deferred.resolve(body)
-                })
-
-                return deferred.promise
-              }
-            }
-          }
+          channel: channelModel(name, nano)
         })
       })
       return deferred.promise
