af203320bdf4998e5beb848c0cba00b6f036d8a1
Ken Grønnbeck <kengroenn@gmail.com>
2014-08-12 22:13:38 +0200

    (feat) Only respond with raw messages

--- modules/commands.js
+++ modules/commands.js
@@ -2,10 +2,9 @@ var _ = require('underscore');
 
 exports.init = function(ws, connections) {
 
-	var establish =  function(connection, raw) {
+	var establish =  function(connection) {
 		console.log('Establiash connection')
 		console.log('  Connection: ' + JSON.stringify(connection))
-		console.log('  Raw: ' + raw )
 
 		var client = connection.client
 		ws.send( JSON.stringify({
@@ -16,23 +15,15 @@ exports.init = function(ws, connections) {
 			channels: connection.channels
 		}))
 
-		var msg = function(msg) {
-			ws.send(JSON.stringify(_.defaults(msg, { key: connection.key })))
-		}
-		, raw = function(msg) {
+		var raw = function(msg) {
 			ws.send(JSON.stringify(_.defaults(msg, { type: 'raw', key: connection.key })))
 		}
 
 		var map = [
-			{ event: 'msg', listener: msg },
 			{ event: 'raw', listener: raw }
 		]
 
-		client.on('msg', msg)
-
-		if (raw) {
-			client.on('raw', raw)
-		}
+		client.on('raw', raw)
 
 		ws.on('close', function() {
 			console.log('Connection closed: ' + connection.key)
@@ -45,17 +36,13 @@ exports.init = function(ws, connections) {
 	, msg = function(message) {
 		if (connections.has(message.key)) {
 			var conn = connections.get(message.key);
-			if (conn.key == message.key) {
-				console.log('weeeelll')
-				console.log(conn)
-				conn.client.emit('send', message);
-			} else {
-				ws.send(JSON.stringify({
-					type: 'error',
-					payload: 'Connection with key ('+ message.key + ')' +
-						'does not exist'
-				}))
-			}
+			conn.client.emit('send', message);
+		} else {
+			ws.send(JSON.stringify({
+				type: 'error',
+				payload: 'Connection with key ('+ message.key + ')' +
+					'does not exist'
+			}))
 		}
 	}
 
@@ -66,6 +53,9 @@ exports.init = function(ws, connections) {
 			, connection = connections.create(config);
 
 			establish(connection, raw);
+
+			console.log("Connectiong to: " + JSON.stringify(message))
+			
 		},
 		reconnect: function(message) {
 			var key = message.key
